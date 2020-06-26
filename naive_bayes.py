7cbde1729b04b5a1c5f152e3d34e1bf71dd3e4e7
alexmjn <a.jenkneary@gmail.com>
2020-06-26 10:37:21 -0400

    More OOP implementation

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,26 +1,74 @@
 """Implements Naive Bayes"""
 import numpy as np
 import pandas as pd
-#from gaussian_pandas import gaussian_fit, gaussian_predict
+from helper import gaussian_numerator
 
 class NaiveBayes:
     def __init__(self, distribution="Gaussian"):
         self.distribution=distribution
-        self.classes = None
-        self.class_freqs = None
-        self.summary_dfs = None
+        self.classes = []
+        self.class_freqs = []
+        self.means_sds = []
 
     def __str__(self):
         print(self.distribution, self.classes, self.class_freqs,
-        self.summary_dfs)
+        self.means_sds)
 
-    # def fit(self, X, y):
-    #     if self.distribution == "Gaussian":
-    #         gaussian_fit(self, X, y)
+    def fit(self, X, y):
+        if self.distribution == "Gaussian":
+               # assert isinstance(naive_bayes, NaiveBayes), "Please pass a valid model"
+            assert isinstance(X, pd.DataFrame), "Please pass a valid data frame."
+        #   assert X.shape[0] == y.shape[0], "The dimensions of the input do not match."
 
-    # def predict(self, X, y):
-    #     if self.distribution == "Gaussian":
-    #         gaussian_predict(self, X, y)
+            # get n, get classes
+            n = X.shape[0]
+            self.classes = y.unique()
+            X["target"] = y
+
+            # split our df by class -- we need to generate sds and means for each
+            # variable for each class.
+
+            for class_type in self.classes:
+                df_subclass = X[X["target"] == class_type]
+                class_freq = df_subclass.shape[0] / n
+                self.class_freqs.append(class_freq)
+
+            # drop supervised column
+            #  so we can generate sds and means without
+            # calculating these over the class column
+
+                df_subclass = df_subclass.drop(["target"], axis=1)
+                subclass_stds = df_subclass.std()
+                subclass_means = df_subclass.mean()
+                means_sds = pd.concat([subclass_means, subclass_stds], axis=1)
+                self.means_sds.append(means_sds)
+
+            return
+
+    def predict(self, X, y):
+        if self.distribution == "Gaussian":
+            assert isinstance(X, pd.DataFrame), "Please pass a valid data frame."
+            assert X.shape[0] == y.shape[0], "The dimensions of the input do not match."
+
+            all_predictions = []
+            for i in range(y.shape[0]):
+                class_preds = []
+
+                for j in range(len(self.classes)):
+                    probs = self.class_freqs[j]
+
+                    for k in range(X.shape[1]):
+                        obs = X.iloc[i][k]
+                        mean = self.means_sds[j][0][k]
+                        sd = self.means_sds[j][1][k]
+                        probs = probs * gaussian_numerator(obs, mean, sd)
+
+                    class_preds.append(probs)
+
+                max_class_index = class_preds.index(max(class_preds))
+                all_predictions.append(self.classes[max_class_index])
+            return pd.Series(all_predictions), class_preds
+
 
 
 if __name__ == "__main__":
@@ -32,13 +80,12 @@ if __name__ == "__main__":
     wiki_target = pd.Series(["male", "male", "male", "male", "female", "female", "female", "female"])
 
     naive_bayes = NaiveBayes()
-    naive_bayes = gaussian_fit(naive_bayes, wiki_df, wiki_target)
+    naive_bayes.fit(wiki_df, wiki_target)
 
     twiki_df = pd.DataFrame()
     twiki_df["height"] = [6]
     twiki_df["weight"] = [130]
     twiki_df["foot_size"] = [8]
-    twiki_target = pd.Series(["male", "male"])
+    twiki_target = pd.Series(["male"])
 
-    print(gaussian_predict(naive_bayes, twiki_df, twiki_target))
-#    [6.197071843878093e-09, 0.0005377909183630022]
+    print(naive_bayes.predict(twiki_df, twiki_target))
