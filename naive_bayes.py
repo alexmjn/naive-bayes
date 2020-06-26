360cac0899c9ee330e0ab23ba5cdbcd8af4bfb9e
alexmjn <a.jenkneary@gmail.com>
2020-06-26 11:01:55 -0400

    final cleanish implementation

--- naive_bayes.py
+++ naive_bayes.py
@@ -10,64 +10,44 @@ class NaiveBayes:
         self.class_freqs = []
         self.means_sds = []
 
-    def __str__(self):
-        print(self.distribution, self.classes, self.class_freqs,
-        self.means_sds)
-
     def fit(self, X, y):
-        if self.distribution == "Gaussian":
-               # assert isinstance(naive_bayes, NaiveBayes), "Please pass a valid model"
-            assert isinstance(X, pd.DataFrame), "Please pass a valid data frame."
-        #   assert X.shape[0] == y.shape[0], "The dimensions of the input do not match."
+        n = X.shape[0]
+        self.classes = y.unique()
+        X["target"] = y
 
-            # get n, get classes
-            n = X.shape[0]
-            self.classes = y.unique()
-            X["target"] = y
+        for class_type in self.classes:
+            df_subclass = X[X["target"] == class_type]
+            class_freq = df_subclass.shape[0] / n
+            self.class_freqs.append(class_freq)
 
-            # split our df by class -- we need to generate sds and means for each
-            # variable for each class.
+            df_subclass = df_subclass.drop(["target"], axis=1)
+            subclass_stds = df_subclass.std()
+            subclass_means = df_subclass.mean()
+            means_sds = pd.concat([subclass_means, subclass_stds], axis=1)
+            self.means_sds.append(means_sds)
 
-            for class_type in self.classes:
-                df_subclass = X[X["target"] == class_type]
-                class_freq = df_subclass.shape[0] / n
-                self.class_freqs.append(class_freq)
-
-            # drop supervised column
-            #  so we can generate sds and means without
-            # calculating these over the class column
-
-                df_subclass = df_subclass.drop(["target"], axis=1)
-                subclass_stds = df_subclass.std()
-                subclass_means = df_subclass.mean()
-                means_sds = pd.concat([subclass_means, subclass_stds], axis=1)
-                self.means_sds.append(means_sds)
-
-            return
+        return
 
     def predict(self, X, y):
-        if self.distribution == "Gaussian":
-            assert isinstance(X, pd.DataFrame), "Please pass a valid data frame."
-            assert X.shape[0] == y.shape[0], "The dimensions of the input do not match."
+        predictions = []
+        for i in range(y.shape[0]):
+            class_preds = []
 
-            all_predictions = []
-            for i in range(y.shape[0]):
-                class_preds = []
+            for j in range(len(self.classes)):
+                probs = self.class_freqs[j]
 
-                for j in range(len(self.classes)):
-                    probs = self.class_freqs[j]
+                for k in range(X.shape[1]):
+                    obs = X.iloc[i][k]
+                    mean = self.means_sds[j][0][k]
+                    sd = self.means_sds[j][1][k]
+                    probs = probs * gaussian_numerator(obs, mean, sd)
 
-                    for k in range(X.shape[1]):
-                        obs = X.iloc[i][k]
-                        mean = self.means_sds[j][0][k]
-                        sd = self.means_sds[j][1][k]
-                        probs = probs * gaussian_numerator(obs, mean, sd)
+                class_preds.append(probs)
 
-                    class_preds.append(probs)
-
-                max_class_index = class_preds.index(max(class_preds))
-                all_predictions.append(self.classes[max_class_index])
-            return pd.Series(all_predictions), class_preds
+            max_class_index = class_preds.index(max(class_preds))
+            predictions.append(self.classes[max_class_index])
+            print(class_preds)
+        return pd.Series(predictions), class_preds
 
 
 
